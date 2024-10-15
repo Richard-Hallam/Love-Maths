eb9beb9cc21b92de8a4a460a7e24d7e991cb9775
Richard Hallam <richardhallam25@gmail.com>
2024-10-15 21:10:25 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,45 @@
-console.log('connected')
+function runGame(){
+
+}
+
+
+function checkAnswer(){
+
+}
+
+
+function calculateCorrectAnswer(){
+
+}
+
+
+function incrementScore(){
+
+}
+
+
+function incrementWrongAnswer(){
+
+}
+
+
+
+function displayAdditionQuestion(){
+
+}
+
+
+function displaySubtractionQuestion(){
+
+}
+
+
+function displayMultiplicationQuestion(){
+
+
+}
+
+
+function displayDivisionQuestion(){
+    
+}
