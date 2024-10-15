473fa8cc681133a2c9ee4b173b16d9b80d3267af
Richard Hallam <richardhallam25@gmail.com>
2024-10-15 21:20:59 +0000

    Added event listeners for button click and dom content load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+//wait for the dom to finish loading before running game
+// get the button elements and add event listeners to them.
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");//gets array of buttons
+
+    for (let button of buttons){//iterates throuhg array of buttons
+        button.addEventListener("click", function(){//add listner for click to each button
+            if(this.getAttribute("data-type") === "submit"){//compares the data type of the button to determine if it is the submit button
+                alert('You clicked submit.');
+            } else{
+                let gameType = this.getAttribute("data-type");//declares game type and sets it to be the data type of the button
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+
 function runGame(){
 
 }
@@ -41,5 +60,5 @@ function displayMultiplicationQuestion(){
 
 
 function displayDivisionQuestion(){
-    
+
 }
